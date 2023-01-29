f6bf15d215e99e7c345b2d8bf43304015bdae541
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-01-29 14:49:36 -0500

    seeds added, setup seed.js

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+const userData = require('./user-seeds.json');
+const postData = require('./post-seeds.json');
+const commentData = require('./comment-seeds.json');
+
+
+const init = async () => {
+  await sequelize.sync({force: true});
+
+  const users = await User.bulkCreate( userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const posts = await Post.bulkCreate(postData, {
+
+  });
+  const comments = await Comment.bulkCreate(commentData, {
+
+  });
+
+  process.exit(0);
+};
+
+init();
