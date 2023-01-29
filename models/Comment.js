026335415e28ad2c96a7bd538f1d358e7dc1feaa
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-01-29 13:51:27 -0500

    setup associations, set up models

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true 
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.STRING, 
+      allowNull: false, 
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+
+  },
+  {
+    sequelize,
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
